d2ca4bbd872bf06f50ae72e59783798bbe301c4e
anorjen <1qwert147@rambler.ru>
2022-04-17 19:13:55 +0300

    Homework 4

--- src/main/java/ru/iteco/accountbank/model/UserDto.java
+++ src/main/java/ru/iteco/accountbank/model/UserDto.java
@@ -2,13 +2,21 @@ package ru.iteco.accountbank.model;
 
 import lombok.Builder;
 import lombok.Data;
+import ru.iteco.accountbank.validation.Create;
+import ru.iteco.accountbank.validation.Update;
+
+import javax.validation.constraints.*;
 
 @Data
 @Builder
 public class UserDto {
 
+    @Null(groups = Create.class)
+    @NotNull(groups = Update.class)
     private Integer id;
+    @NotBlank
     private String name;
+    @Email
     private String email;
 
 }
